92e197507363ce7b68345dbe7cf601d8b1ffb098
SVM <66192765+laoSVM@users.noreply.github.com>
2023-03-23 14:24:24 +0000

    Version 1

--- main.py
+++ main.py
@@ -1,55 +1,99 @@
-<<<<<<< HEAD
-import os
 import argparse
-import json
-=======
-# import os
-import argparse
->>>>>>> origin/master
+import csv
 import requests
+from random import randint
+import time
+from search_jobs import Scraper
 
-if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description="Find Nearby or Faraway Jobs")
+def test(
+    location="New York", profession="Data Engineer", start=0, end=1000
+):
+    """test function"""
+    job_postings = []
+    while start < end:
+        time.sleep(randint(0, 5) * 1)
+        print(f"Testing {start}")
+        try:
+            url = (
+                f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search/?keywords={profession}"
+                + (f"&location={location}" if location else "")
+                + f"&start={start}"
+            )
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+            result = Scraper().search_results_parsing(response.text)
+            job_postings.extend(result)
+            start += 25
+        except requests.HTTPError as err:
+            print(f"[!!] Something went wrong at {start}! {err}")
+            break
+    return job_postings
 
-    parser.add_argument("-p", "--place",
-                        nargs='+', metavar='PLACES',
-                        action="store",
-                        help="Enter country/city/state. One or more places to look jobs from."
-                    )
 
-    parser.add_argument("-j", '--jobfunction',
-                        nargs='+',
-                        metavar='jobfunction',
-                        action='store',
-                        help='Searches Job Specification in your area. (e.g software-engineer)'
-                    )
+def write_to_file(job_postings, destination: str):
+    """write to csv file"""
+    with open(destination, "w") as f:
+        fieldnames = job_postings[0].keys()
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
 
-    parser.add_argument("-jp", '--jobplace',
-                        nargs=2,
-                        metavar=('job', 'place'),
-                        action='store',
-                        help="Searches The Specified Job in the Specified Place. (e.g teacher iowa)"
-                    )
+        writer.writeheader()
+        for row in job_postings:
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="Find Nearby or Faraway Jobs",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--place",
+        default="New York",
+        nargs="+",
+        metavar="PLACES",
+        action="store",
+        help="Enter country/city/state. One or more places to look jobs from.",
+    )
+
+    parser.add_argument(
+        "-j",
+        "--jobfunction",
+        default="Data Engineer",
+        nargs="+",
+        metavar="jobfunction",
+        action="store",
+        help="Searches Job Specification in your area. (e.g software-engineer)",
+    )
+
+    parser.add_argument(
+        "-jp",
+        "--jobplace",
+        nargs=2,
+        metavar=("job", "place"),
+        action="store",
+        help="Searches The Specified Job in the Specified Place. (e.g teacher iowa)",
+    )
     args = parser.parse_args()
 
     if args.place:
         place = [args.place]
-
         print(place)
 
-
     if args.jobfunction:
-        with requests.get('https://ipinfo.io/', timeout=10) as response:
-            place = response.json()['city'].replace('"', '')
+        with requests.get("https://ipinfo.io/", timeout=10) as response:
+            place = response.json()["city"].replace('"', "")
             response.raise_for_status()
 
         job = [args.jobfunction]
-        print(job, '\n', place)
-
+        print(job, "\n", place)
 
     if args.jobplace:
         jp = [args.jobplace]
         job, place = jp[0], jp[1]
-        print(job, '/n', place)
+        print(job, "/n", place)
+
+    job_postings = test(location=place, profession=job, start=0, end=1000)
+    write_to_file(job_postings, "jobs.csv")
--- search_jobs.py
+++ search_jobs.py
@@ -1,14 +1,10 @@
-import csv
-import os
 import argparse
 import re
-import json
 import requests
-import colorama
 from bs4 import BeautifulSoup
 from random import randint
 import time
-import read_data
+
 
 
 class Test:
@@ -216,26 +212,27 @@ class JdCrawler:
         result_dict["job_description"] = job_description[: job_description.find("\n")]
         return result_dict
 
-class Scraper:
-    def __init__(self, html: str):
-        self.html = html
 
-    def web_parsing(self) -> list:
+class Scraper:
+    """The base scraper for LinkedIn search results"""
+    def __init__(self):
+        pass
+
+    def search_results_parsing(self, html: str) -> list:
         # create beautifulsoup
-        soup = BeautifulSoup(self.html, "html.parser")
-        search_results = soup.find(
-            "ul", {"class": "jobs-search__results-list"}
-        )  # under <section class="two-pane-serp-page__results-list">
+        soup = BeautifulSoup(html, "html.parser")
 
         result_list = []
         progress_bar = 0
-        for result in search_results.find_all("li"):
+        for result in soup.find_all("li"):
             print(f"Searching jobs offset:{progress_bar}")
             progress_bar += 1
 
             result_dict = {}
             # RE match Job ID
-            url = result.find("a", {"class": "base-card__full-link"})["href"]
+            url = result.find("h3", {"class": "base-search-card__title"}).find_previous(
+                "a"
+            )["href"]
             match = re.search(r"(\d+)\?", url)
             # Check if there is a match
             if match:
@@ -277,263 +274,8 @@ class Scraper:
                 result_dict["Publish Date"] = None
 
             result_list.append(result_dict)
-        
+
         return result_list
-
-
-
-
-
-class Scrape_Place:
-    def __init__(self, location):
-        self.location = location
-
-    def web_parsing_location(self):
-        try:
-            response = requests.get(
-                f"https://www.linkedin.com/jobs/search/?keywords={self.location}",
-                # f'https://www.linkedin.com/jobs/jobs-in-{self.location}?trk=homepage-basic_intent-module-jobs&position=1&pageNum=0',
-                timeout=10,
-            )
-            response.raise_for_status()
-
-            # create beautifulsoup
-            soup = BeautifulSoup(response.text, "html.parser")
-
-            return extract_job_links(soup)
-        except requests.HTTPError as err:
-            print(f"[!!] Something went wrong! {err}")
-
-
-class Scrape_Profession:
-    def __init__(self, profession, city):
-        self.profession = profession
-        self.city = city
-
-    def profession_current_location(self):
-        try:
-            req = requests.get(
-                f"https://www.linkedin.com/jobs/search?keywords={self.profession}&location={self.city}&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0"
-            )
-            req.raise_for_status()
-
-            # create Soup
-            page_soup = soup(req.text, "html.parser")
-            return extract_job_links(page_soup)
-        except requests.HTTPError as err:
-            print(
-                colorama.Fore.RED,
-                f"[!!] Something went wrong! {err}",
-                colorama.Style.RESET_ALL,
-            )
-
-
-class Profession_Location:
-    def __init__(self, profession, place):
-        self.profession = profession
-        self.place = place
-
-    def profession_location(self):
-        try:
-            req = requests.get(
-                f"https://www.linkedin.com/jobs/search?keywords={self.profession}&location={self.place}&geoId=&trk=homepage-jobseeker_jobs-search-bar_search-submit&redirect=false&position=1&pageNum=0"
-            )
-            req.raise_for_status()
-
-            # create Soup
-            page_soup = soup(req.text, "html.parser")
-            return extract_job_links(page_soup)
-        except requests.HTTPError as err:
-            print(
-                colorama.Fore.RED,
-                f"[!!] Something went wrong! {err}",
-                colorama.Style.RESET_ALL,
-            )
-
-
-def scrape_write(links):
-    try:
-        if "-" in job:
-            formatting = [x.capitalize() for x in job.split("-")]
-            my_job = " ".join(formatting)
-        else:
-            my_job = job.capitalize()
-
-        print(
-            colorama.Fore.YELLOW,
-            f"[!] There are {len(links)} available {my_job} jobs in {place.capitalize()}.\n",
-            colorama.Style.RESET_ALL,
-        )
-
-        csv_filename = f"jobs_in_{place}.csv"
-        with open(os.path.join(folder_name, csv_filename), "w", encoding="utf-8") as f:
-            headers = [
-                "Source",
-                "Organization",
-                "Job Title",
-                "Location",
-                "Posted",
-                "Applicants Hired",
-                "Seniority Level",
-                "Employment Type",
-                "Job Function",
-                "Industries",
-            ]
-            write = csv.writer(f, dialect="excel")
-            write.writerow(headers)
-
-            for job_link in links:
-                page_req = requests.get(job_link)
-                page_req.raise_for_status()
-
-                # Parse HTML
-                job_soup = soup(page_req.text, "html.parser")
-                my_data = [job_link]
-
-                # Topcard scraping
-                for content in job_soup.findAll(
-                    "div", {"class": "topcard__content-left"}
-                )[0:]:
-                    # Scraping Organization Names
-                    orgs = {
-                        "Default-Org": [
-                            org.text
-                            for org in content.findAll(
-                                "a",
-                                {
-                                    "class": "topcard__org-name-link topcard__flavor--black-link"
-                                },
-                            )
-                        ],
-                        "Flavor-Org": [
-                            org.text
-                            for org in content.findAll(
-                                "span", {"class": "topcard__flavor"}
-                            )
-                        ],
-                    }
-
-                    if orgs["Default-Org"] == []:
-                        org = orgs["Flavor-Org"][0]
-                        my_data.append(org)
-                    else:
-                        for org in orgs["Default-Org"]:
-                            my_data.append(org)
-
-                    # Scraping Job Title
-                    for title in content.findAll("h1", {"class": "topcard__title"})[0:]:
-                        print(
-                            colorama.Fore.GREEN,
-                            f"[*] {title.text}",
-                            colorama.Style.RESET_ALL,
-                            colorama.Fore.YELLOW,
-                            f"- {org}",
-                            colorama.Style.RESET_ALL,
-                        )
-                        my_data.append(title.text.replace(",", "."))
-
-                    for location in content.findAll(
-                        "span", {"class": "topcard__flavor topcard__flavor--bullet"}
-                    )[0:]:
-                        my_data.append(location.text.replace(",", "."))
-
-                    # Scraping Job Time Posted
-                    posts = {
-                        "Old": [
-                            posted.text
-                            for posted in content.findAll(
-                                "span",
-                                {
-                                    "class": "topcard__flavor--metadata posted-time-ago__text"
-                                },
-                            )
-                        ],
-                        "New": [
-                            posted.text
-                            for posted in content.findAll(
-                                "span",
-                                {
-                                    "class": "topcard__flavor--metadata posted-time-ago__text posted-time-ago__text--new"
-                                },
-                            )
-                        ],
-                    }
-
-                    if posts["New"] == []:
-                        for text in posts["Old"]:
-                            my_data.append(text)
-                    else:
-                        for text in posts["New"]:
-                            my_data.append(text)
-
-                    # Scraping Number of Applicants Hired
-                    applicants = {
-                        "More-Than": [
-                            applicant.text
-                            for applicant in content.findAll(
-                                "figcaption", {"class": "num-applicants__caption"}
-                            )
-                        ],
-                        "Current": [
-                            applicant.text
-                            for applicant in content.findAll(
-                                "span",
-                                {
-                                    "class": "topcard__flavor--metadata topcard__flavor--bullet num-applicants__caption"
-                                },
-                            )
-                        ],
-                    }
-
-                    if applicants["Current"] == []:
-                        for applicant in applicants["More-Than"]:
-                            my_data.append(f"{get_nums(applicant)}+ Applicants")
-                    else:
-                        for applicant in applicants["Current"]:
-                            my_data.append(f"{get_nums(applicant)} Applicants")
-
-                # Criteria scraping
-                for criteria in job_soup.findAll(
-                    "span", {"class": "job-criteria__text job-criteria__text--criteria"}
-                )[:4]:
-                    my_data.append(criteria.text)
-
-                write.writerows([my_data])
-
-            print(
-                colorama.Fore.YELLOW,
-                f"\n\n[!] Written all information in: {csv_filename}",
-                colorama.Style.RESET_ALL,
-            )
-        # Reads scraped data
-        read_data.read_scraped(folder_name, csv_filename)
-    except requests.HTTPError as err:
-        print(
-            colorama.Fore.RED,
-            f"[!!] Something went wrong! {err}",
-            colorama.Style.RESET_ALL,
-        )
-
-
-def get_nums(string):
-    a_list = string.split()
-    for num in a_list:
-        if num.isdigit():
-            return num
-
-
-def extract_job_links(cursor):
-    print("Extracting job links")
-    job_links = []
-
-    for res_card in cursor.findAll("ul", {"class": "scaffold-layout__list-container"}):
-        print(res_card)
-        job_links.append(res_card)
-
-    return job_links
-    # return scrape_write(job_links)
-
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
