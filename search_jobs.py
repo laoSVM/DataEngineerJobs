a95a812c53ac45f2106127a06a739ded07eb8217
SVM <66192765+laoSVM@users.noreply.github.com>
2023-03-24 15:47:06 +0000

    Update

--- search_jobs.py
+++ search_jobs.py
@@ -6,219 +6,202 @@ from random import randint
 import time
 
 
+# class Test:
+#     def __init__(self, location="New York", profession="Data Engineer"):
+#         self.location = location
+#         self.profession = profession
 
-class Test:
-    def __init__(self, location="New York", profession="Data Engineer"):
-        self.location = location
-        self.profession = profession
+#     def web_scraper_dep(self):
+#         """从 base-search-card__info 获取基本信息, 但是Job ID 在上一个层级无法获取到"""
+#         try:
+#             response = requests.get(
+#                 f"https://www.linkedin.com/jobs/search/?keywords={self.location}",
+#                 timeout=10,
+#             )
+#             response.raise_for_status()
+#         except requests.HTTPError as err:
+#             print(f"[!!] Something went wrong! {err}")
+#             return None
+#         # create beautifulsoup
+#         soup = BeautifulSoup(response.text, "html.parser")
+#         result_list = []
+#         for result in soup.find_all("div", {"class": "base-search-card__info"}):
+#             result_dict = {}
+#             result_dict["Job Title"] = result.find(
+#                 "h3", {"class": "base-search-card__title"}
+#             ).text.strip()
+#             result_dict["Location"] = result.find(
+#                 "span", {"class": "job-search-card__location"}
+#             ).text.strip()
+#             time_tag = result.find("time", {"class": "job-search-card__listdate"})
+#             time_tag_new = result.find(
+#                 "time", {"class": "job-search-card__listdate--new"}
+#             )
+#             if time_tag is not None:
+#                 result_dict["Publish Date"] = time_tag["datetime"]
+#             elif time_tag_new is not None:
+#                 result_dict["Publish Date"] = time_tag_new["datetime"]
+#             else:
+#                 result_dict["Publish Date"] = None
+#             result_list.append(result_dict)
 
-    def web_scraper_dep(self):
-        """从 base-search-card__info 获取基本信息, 但是Job ID 在上一个层级无法获取到"""
-        try:
-            response = requests.get(
-                f"https://www.linkedin.com/jobs/search/?keywords={self.location}",
-                timeout=10,
-            )
-            response.raise_for_status()
-        except requests.HTTPError as err:
-            print(f"[!!] Something went wrong! {err}")
-            return None
-        # create beautifulsoup
-        soup = BeautifulSoup(response.text, "html.parser")
-        result_list = []
-        for result in soup.find_all("div", {"class": "base-search-card__info"}):
-            result_dict = {}
-            result_dict["Job Title"] = result.find(
-                "h3", {"class": "base-search-card__title"}
-            ).text.strip()
-            result_dict["Location"] = result.find(
-                "span", {"class": "job-search-card__location"}
-            ).text.strip()
-            time_tag = result.find("time", {"class": "job-search-card__listdate"})
-            time_tag_new = result.find(
-                "time", {"class": "job-search-card__listdate--new"}
-            )
-            if time_tag is not None:
-                result_dict["Publish Date"] = time_tag["datetime"]
-            elif time_tag_new is not None:
-                result_dict["Publish Date"] = time_tag_new["datetime"]
-            else:
-                result_dict["Publish Date"] = None
-            result_list.append(result_dict)
+#         for i in result_list[:5]:
+#             print(i, "\n")
 
-        for i in result_list[:5]:
-            print(i, "\n")
+#         print("Job Finished")
+#         return None
 
-        print("Job Finished")
-        return None
+#     def web_scraper_dep_2(self):
+#         try:
+#             response = requests.get(
+#                 f"https://www.linkedin.com/jobs/search/?keywords={self.location}",
+#                 timeout=10,
+#             )
+#             response.raise_for_status()
+#         except requests.HTTPError as err:
+#             print(f"[!!] Something went wrong! {err}")
+#             return None
+#         # create beautifulsoup
+#         soup = BeautifulSoup(response.text, "html.parser")
+#         search_results = soup.find("ul", {"class": "jobs-search__results-list"})
+#         result_list = []
+#         for result in search_results.find_all("li"):
+#             result_dict = {}
+#             # RE match Job ID
+#             url = result.find("a", {"class": "base-card__full-link"})["href"]
+#             match = re.search(r"(\d+)\?", url)
+#             # Check if there is a match
+#             if match:
+#                 # print(match.group(1)) # 3529282119
+#                 result_dict["Job ID"] = match.group(1)
+#             else:
+#                 # Print an error message
+#                 print("No match found")
+#                 result_dict["Job ID"] = None
 
-    def web_scraper_dep_2(self):
-        try:
-            response = requests.get(
-                f"https://www.linkedin.com/jobs/search/?keywords={self.location}",
-                timeout=10,
-            )
-            response.raise_for_status()
-        except requests.HTTPError as err:
-            print(f"[!!] Something went wrong! {err}")
-            return None
-        # create beautifulsoup
-        soup = BeautifulSoup(response.text, "html.parser")
-        search_results = soup.find("ul", {"class": "jobs-search__results-list"})
-        result_list = []
-        for result in search_results.find_all("li"):
-            result_dict = {}
-            # RE match Job ID
-            url = result.find("a", {"class": "base-card__full-link"})["href"]
-            match = re.search(r"(\d+)\?", url)
-            # Check if there is a match
-            if match:
-                # print(match.group(1)) # 3529282119
-                result_dict["Job ID"] = match.group(1)
-            else:
-                # Print an error message
-                print("No match found")
-                result_dict["Job ID"] = None
+#             result_dict["Job Title"] = result.find(
+#                 "h3", {"class": "base-search-card__title"}
+#             ).text.strip()
+#             result_dict["Location"] = result.find(
+#                 "span", {"class": "job-search-card__location"}
+#             ).text.strip()
+#             time_tag = result.find("time", {"class": "job-search-card__listdate"})
+#             time_tag_new = result.find(
+#                 "time", {"class": "job-search-card__listdate--new"}
+#             )
+#             if time_tag is not None:
+#                 result_dict["Publish Date"] = time_tag["datetime"]
+#             elif time_tag_new is not None:
+#                 result_dict["Publish Date"] = time_tag_new["datetime"]
+#             else:
+#                 result_dict["Publish Date"] = None
+#             result_list.append(result_dict)
 
-            result_dict["Job Title"] = result.find(
-                "h3", {"class": "base-search-card__title"}
-            ).text.strip()
-            result_dict["Location"] = result.find(
-                "span", {"class": "job-search-card__location"}
-            ).text.strip()
-            time_tag = result.find("time", {"class": "job-search-card__listdate"})
-            time_tag_new = result.find(
-                "time", {"class": "job-search-card__listdate--new"}
-            )
-            if time_tag is not None:
-                result_dict["Publish Date"] = time_tag["datetime"]
-            elif time_tag_new is not None:
-                result_dict["Publish Date"] = time_tag_new["datetime"]
-            else:
-                result_dict["Publish Date"] = None
-            result_list.append(result_dict)
+#         for i in result_list[:5]:
+#             print(i, "\n")
 
-        for i in result_list[:5]:
-            print(i, "\n")
+#         print("Job Finished")
+#         return None
 
-        print("Job Finished")
-        return None
+#     def web_scraper(self):
+#         try:
+#             url = (
+#                 f"https://www.linkedin.com/jobs/search/?keywords={self.profession}"
+#                 + (f"&location={self.location}" if self.location else "")
+#                 + f"&start={25*randint(0,5)}"
+#             )
+#             response = requests.get(url, timeout=10)
+#             response.raise_for_status()
+#         except requests.HTTPError as err:
+#             print(f"[!!] Something went wrong! {err}")
+#             return None
+#         # create beautifulsoup
+#         soup = BeautifulSoup(response.text, "html.parser")
+#         search_results = soup.find(
+#             "ul", {"class": "jobs-search__results-list"}
+#         )  # under <section class="two-pane-serp-page__results-list">
 
-    def web_scraper(self):
-        try:
-            url = (
-                f"https://www.linkedin.com/jobs/search/?keywords={self.profession}"
-                + (f"&location={self.location}" if self.location else "")
-                + f"&start={25*randint(0,5)}"
-            )
-            response = requests.get(url, timeout=10)
-            response.raise_for_status()
-        except requests.HTTPError as err:
-            print(f"[!!] Something went wrong! {err}")
-            return None
-        # create beautifulsoup
-        soup = BeautifulSoup(response.text, "html.parser")
-        search_results = soup.find(
-            "ul", {"class": "jobs-search__results-list"}
-        )  # under <section class="two-pane-serp-page__results-list">
+#         result_list = []
+#         progress_bar = 0
+#         for result in search_results.find_all("li"):
+#             print(f"Searching jobs offset:{progress_bar}")
+#             progress_bar += 1
 
-        result_list = []
-        progress_bar = 0
-        for result in search_results.find_all("li"):
-            print(f"Searching jobs offset:{progress_bar}")
-            progress_bar += 1
+#             result_dict = {}
+#             # RE match Job ID
+#             url = result.find("a", {"class": "base-card__full-link"})["href"]
+#             job_description_dict = JdCrawler(url).web_scraper()  # get job description
+#             time.sleep(randint(0, 5) * 0.1)
+#             match = re.search(r"(\d+)\?", url)
+#             # Check if there is a match
+#             if match:
+#                 # print(match.group(1)) # 3529282119
+#                 result_dict["Job ID"] = match.group(1)
+#             else:
+#                 # Print an error message
+#                 print("No match found")
+#                 result_dict["Job ID"] = None
+#             # Writing data to a dictionary
+#             result_dict["Job Title"] = result.find(
+#                 "h3", {"class": "base-search-card__title"}
+#             ).text.strip()
+#             result_dict["Company Name"] = result.find(
+#                 "h4", {"class": "base-search-card__subtitle"}
+#             ).text.strip()
+#             result_dict["Location"] = result.find(
+#                 "span", {"class": "job-search-card__location"}
+#             ).text.strip()
+#             # Salary
+#             salary_info = result.find("span", {"class": "job-search-card__salary-info"})
+#             result_dict["Salary"] = (
+#                 re.sub(re.compile(r"\s+"), "", salary_info.text)
+#                 if salary_info is not None
+#                 else None
+#             )
+#             # Publish Date
+#             time_tag = result.find("time", {"class": "job-search-card__listdate"})
+#             time_tag_new = result.find(
+#                 "time", {"class": "job-search-card__listdate--new"}
+#             )
+#             if time_tag is not None:
+#                 result_dict["Publish Date"] = time_tag["datetime"]
+#             elif time_tag_new is not None:
+#                 result_dict["Publish Date"] = time_tag_new["datetime"]
+#             else:
+#                 result_dict["Publish Date"] = None
 
-            result_dict = {}
-            # RE match Job ID
-            url = result.find("a", {"class": "base-card__full-link"})["href"]
-            job_description_dict = JdCrawler(url).web_scraper()  # get job description
-            time.sleep(randint(0, 5) * 0.1)
-            match = re.search(r"(\d+)\?", url)
-            # Check if there is a match
-            if match:
-                # print(match.group(1)) # 3529282119
-                result_dict["Job ID"] = match.group(1)
-            else:
-                # Print an error message
-                print("No match found")
-                result_dict["Job ID"] = None
-            # Writing data to a dictionary
-            result_dict["Job Title"] = result.find(
-                "h3", {"class": "base-search-card__title"}
-            ).text.strip()
-            result_dict["Company Name"] = result.find(
-                "h4", {"class": "base-search-card__subtitle"}
-            ).text.strip()
-            result_dict["Location"] = result.find(
-                "span", {"class": "job-search-card__location"}
-            ).text.strip()
-            # Salary
-            salary_info = result.find("span", {"class": "job-search-card__salary-info"})
-            result_dict["Salary"] = (
-                re.sub(re.compile(r"\s+"), "", salary_info.text)
-                if salary_info is not None
-                else None
-            )
-            # Publish Date
-            time_tag = result.find("time", {"class": "job-search-card__listdate"})
-            time_tag_new = result.find(
-                "time", {"class": "job-search-card__listdate--new"}
-            )
-            if time_tag is not None:
-                result_dict["Publish Date"] = time_tag["datetime"]
-            elif time_tag_new is not None:
-                result_dict["Publish Date"] = time_tag_new["datetime"]
-            else:
-                result_dict["Publish Date"] = None
+#             result_list.append(dict(result_dict, **job_description_dict))
 
-            result_list.append(dict(result_dict, **job_description_dict))
+#         for i in result_list[:5]:
+#             print(i, "\n")
 
-        for i in result_list[:5]:
-            print(i, "\n")
-
-        # with open("data.json", "w") as f:
-        #     json.dump(result_list, f)
-        print("Job Finished")
-        return None
-
-
-class JdCrawler:
-    def __init__(self, job_url: str):
-        self.job_url = job_url
-
-    def web_scraper(self) -> dict:
-        if self.job_url is None:
-            return {"Job Description": None}
-        try:
-            response = requests.get(self.job_url, timeout=10)
-            response.raise_for_status()
-        except requests.HTTPError as err:
-            print(f"[!!] Something went wrong! {err}")
-            return None
-        # create beautifulsoup
-        soup = BeautifulSoup(response.text, "html.parser")
-        job_description = soup.find("div", {"class": "description__text"}).text.strip()
-        criteria_list = soup.find("ul", {"class": "description__job-criteria-list"})
-
-        result_dict = {}
-        for criteria in criteria_list.find_all("li"):
-            subheader = criteria.find(
-                "h3", {"class": "description__job-criteria-subheader"}
-            ).text.strip()
-            value = criteria.find(
-                "span", {"class": "description__job-criteria-text"}
-            ).text.strip()
-            result_dict[subheader] = value
-        result_dict["job_description"] = job_description[: job_description.find("\n")]
-        return result_dict
+#         # with open("data.json", "w") as f:
+#         #     json.dump(result_list, f)
+#         print("Job Finished")
+#         return None
 
 
 class Scraper:
     """The base scraper for LinkedIn search results"""
+
     def __init__(self):
         pass
 
+    def cook_soup(self, url: str, destination: str) -> None:
+        """Save the source code of the webpage to a file"""
+        try:
+            response = requests.get(url, timeout=10)
+            response.raise_for_status()
+        except requests.HTTPError as err:
+            print(f"[!!] Something went wrong! {err}")
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        with open(destination, "w", encoding="utf-8") as f:
+            f.write(soup.prettify())
+        print(f"Source code saved to {destination}")
+
     def search_results_parsing(self, html: str) -> list:
+        """Parse the search results from response.text, and return a list of job info dict"""
         # create beautifulsoup
         soup = BeautifulSoup(html, "html.parser")
 
@@ -277,6 +260,26 @@ class Scraper:
 
         return result_list
 
+    def jd_parsing(self, html: str) -> dict:
+        """Parse the jd page from response.text, and return a jd dict"""
+        soup = BeautifulSoup(html, "html.parser")
+
+        job_description = soup.find("div", {"class": "description__text"}).text.strip()
+        criteria_list = soup.find("ul", {"class": "description__job-criteria-list"})
+
+        result_dict = {}
+        for criteria in criteria_list.find_all("li"):
+            subheader = criteria.find(
+                "h3", {"class": "description__job-criteria-subheader"}
+            ).text.strip()
+            value = criteria.find(
+                "span", {"class": "description__job-criteria-text"}
+            ).text.strip()
+            result_dict[subheader] = value
+        result_dict["job_description"] = job_description[: job_description.find("\n")]
+        return result_dict
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -291,7 +294,3 @@ if __name__ == "__main__":
     print(f"Searching for: Location: {location}, Profession: {profession}")
 
     Test(location, profession).web_scraper()
-
-    # JdCrawler(
-    #     "https://www.linkedin.com/jobs/view/data-engineer-analytics-generalist-at-instagram-3512575738"
-    # ).web_scraper()
